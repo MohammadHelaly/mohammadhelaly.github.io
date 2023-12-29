308b33ea2d5692642a541e561b4f7831d96fc5d0
MohammadHelaly <mohammad.helaly@gmail.com>
2023-12-29 19:37:07 +0200

    Updates

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import styles from "./NavBar.module.css";
 import useAnimate from "../../hooks/use-animate";
 
-const Navbar = () => {
-	const navBarRef = useAnimate("animate-navbar", false);
-	const contentRef = useAnimate("animate-navbar", false);
+const NavBar = () => {
+	const logoRef = useAnimate("animate", false);
+	const listRef = useAnimate("animate", false);
+	const buttonRef = useAnimate("animate", false);
 	const [scrolled, setScrolled] = useState(false);
 
 	const handleScroll = () => {
@@ -24,64 +25,168 @@ const Navbar = () => {
 
 	return (
 		<nav
-			ref={navBarRef}
-			className={`navbar navbar-expand-md navbar-light fixed-top 
-				${scrolled ? styles["top-nav-scrolled"] : styles["top-nav"]}`}>
-			<div
-				ref={contentRef}
-				className={`${styles["top-nav-content"]} container topnav-container`}>
+			className={`navbar navbar-expand-md bg-white fixed-top 
+			${scrolled ? styles["top-nav-scrolled"] : styles["top-nav"]}`}>
+			<div className="container">
 				<a
-					className={`navbar-brand mb-1 text-dark ${styles["logo"]} ${styles["top-nav-link"]}`}
+					ref={logoRef}
+					className={`navbar-brand mb-1 text-dark ${styles["logo"]} ${styles["top-nav-content"]} ${styles["top-nav-link"]}`}
 					href="#home">
 					helaly.dev
 				</a>
 				<button
-					className="navbar-toggler"
+					ref={buttonRef}
+					className={`navbar-toggler ${styles["top-nav-content"]}`}
 					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
+					data-bs-toggle="offcanvas"
+					data-bs-target="#offcanvasNavbar"
+					aria-controls="offcanvasNavbar"
 					aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div
-					className="collapse navbar-collapse"
-					id="navbarSupportedContent">
-					<ul className="navbar-nav ms-auto text-center">
-						<li className="nav-item">
+					className={`offcanvas offcanvas-end ${styles["offcanvas-content"]}`}
+					tabIndex="-1"
+					id="offcanvasNavbar"
+					aria-labelledby="offcanvasNavbarLabel">
+					<div className="offcanvas-header">
+						<h5
+							className="offcanvas-title"
+							id="offcanvasNavbarLabel">
 							<a
-								className={`nav-link custom-link ${styles["top-nav-link"]}`}
-								href="#about">
-								About
+								className={`navbar-brand text-dark ${styles["logo"]} ${styles["top-nav-link"]}`}
+								href="#home">
+								helaly.dev
 							</a>
-						</li>
-						<li className="nav-item">
-							<a
-								className={`nav-link custom-link ${styles["top-nav-link"]}`}
-								href="#projects">
-								Projects
-							</a>
-						</li>
-						<li className="nav-item">
-							<a
-								className={`nav-link custom-link ${styles["top-nav-link"]}`}
-								href="#skills">
-								Skills
-							</a>
-						</li>
-						<li className="nav-item">
-							<a
-								className={`nav-link custom-link ${styles["top-nav-link"]}`}
-								href="#contact">
-								Contact
-							</a>
-						</li>
-					</ul>
+						</h5>
+						<button
+							type="button"
+							className="btn-close"
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"></button>
+					</div>
+					<div className="offcanvas-body">
+						<ul
+							ref={listRef}
+							className={`navbar-nav justify-content-end flex-grow-1 ${styles["top-nav-list"]}`}>
+							<li className="nav-item">
+								<a
+									className={`nav-link custom-link ${styles["top-nav-link"]}`}
+									href="#about">
+									About
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className={`nav-link custom-link ${styles["top-nav-link"]}`}
+									href="#projects">
+									Projects
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className={`nav-link custom-link ${styles["top-nav-link"]}`}
+									href="#skills">
+									Skills
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className={`nav-link custom-link ${styles["top-nav-link"]}`}
+									href="#contact">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</nav>
 	);
 };
 
-export default Navbar;
+export default NavBar;
+
+// const Navbar = () => {
+// 	const navBarRef = useAnimate("animate-navbar", false);
+// 	const contentRef = useAnimate("animate-navbar", false);
+// 	const [scrolled, setScrolled] = useState(false);
+
+// 	const handleScroll = () => {
+// 		if (window.scrollY > 100) {
+// 			setScrolled(true);
+// 		} else {
+// 			setScrolled(false);
+// 		}
+// 	};
+
+// 	useEffect(() => {
+// 		window.addEventListener("scroll", handleScroll);
+// 		return () => {
+// 			window.removeEventListener("scroll", handleScroll);
+// 		};
+// 	}, []);
+
+// 	return (
+// 		<nav
+// 			ref={navBarRef}
+// 			className={`navbar navbar-expand-md navbar-light fixed-top
+// 				${scrolled ? styles["top-nav-scrolled"] : styles["top-nav"]}`}>
+// 			<div
+// 				ref={contentRef}
+// 				className={`${styles["top-nav-content"]} container topnav-container`}>
+// 				<a
+// 					className={`navbar-brand mb-1 text-dark ${styles["logo"]} ${styles["top-nav-link"]}`}
+// 					href="#home">
+// 					helaly.dev
+// 				</a>
+// 				<button
+// 					className="navbar-toggler"
+// 					type="button"
+// 					data-bs-toggle="collapse"
+// 					data-bs-target="#navbarSupportedContent"
+// 					aria-controls="navbarSupportedContent"
+// 					aria-expanded="false"
+// 					aria-label="Toggle navigation">
+// 					<span className="navbar-toggler-icon"></span>
+// 				</button>
+// 				<div
+// 					className="collapse navbar-collapse"
+// 					id="navbarSupportedContent">
+// 					<ul className="navbar-nav ms-auto text-center">
+// 						<li className="nav-item">
+// 							<a
+// 								className={`nav-link custom-link ${styles["top-nav-link"]}`}
+// 								href="#about">
+// 								About
+// 							</a>
+// 						</li>
+// 						<li className="nav-item">
+// 							<a
+// 								className={`nav-link custom-link ${styles["top-nav-link"]}`}
+// 								href="#projects">
+// 								Projects
+// 							</a>
+// 						</li>
+// 						<li className="nav-item">
+// 							<a
+// 								className={`nav-link custom-link ${styles["top-nav-link"]}`}
+// 								href="#skills">
+// 								Skills
+// 							</a>
+// 						</li>
+// 						<li className="nav-item">
+// 							<a
+// 								className={`nav-link custom-link ${styles["top-nav-link"]}`}
+// 								href="#contact">
+// 								Contact
+// 							</a>
+// 						</li>
+// 					</ul>
+// 				</div>
+// 			</div>
+// 		</nav>
+// 	);
+// };
+
+// export default Navbar;
